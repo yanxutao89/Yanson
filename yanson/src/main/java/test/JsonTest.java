811136ddb9e89932de1c144891bb7eb0b21d2a2d
yanxutao89 <910135896@qq.com>
2020-09-01 07:21:21 +0800

    feat:增加测试样例

--- yanson/src/main/java/test/JsonTest.java
+++ yanson/src/main/java/test/JsonTest.java
@@ -40,7 +40,7 @@ public class JsonTest {
 		for (String json : jsons) {
 			JSON_LIST.add(json.trim());
 		}
-		jsonStr = JSON_LIST.get(4);
+		jsonStr = JSON_LIST.get(6);
 	}
 
 	@MyTest(count = 1, name = "Yanson")
